dd2dc5d87a454055e0f1326509e223dda332d0bf
Eshwari Bhide <ebhide99@gmail.com>
2022-04-10 19:02:06 -0400

    Making leaderboard dynamic

--- app/src/main/java/edu/neu/madcourse/numad22sp_moviereviewer_teamdedj/LeaderboardActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_moviereviewer_teamdedj/LeaderboardActivity.java
@@ -1,10 +1,15 @@
 package edu.neu.madcourse.numad22sp_moviereviewer_teamdedj;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +17,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class LeaderboardActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
     private static final String TAG = "LeaderboardActivity";
     private RecyclerView recyclerView;
     private LeaderboardRecyclerViewAdapter recyclerViewAdapter;
@@ -19,23 +25,17 @@ public class LeaderboardActivity extends AppCompatActivity {
     // private ArrayList<LeaderboardActivity.LeaderboardItem> userItems = new ArrayList<>();
 
     public static class LeaderboardItem {
-        public final int userId;
         public final String userName;
         public final int userReviewCount;
-        public final int badgeLevel;
+        public final String badgeLevel;
 
-        public LeaderboardItem(int userId, String userName, int userReviewCount, int badgeLevel) {
+        public LeaderboardItem(String userName, int userReviewCount, String badgeLevel) {
             this.userName = userName;
             this.userReviewCount = userReviewCount;
             this.badgeLevel = badgeLevel;
-            this.userId = userId;
         }
 
-        public int getUserId() {
-            return userId;
-        }
-
-        public int getBadgeLevel() {
+        public String getBadgeLevel() {
             return badgeLevel;
         }
 
@@ -50,22 +50,38 @@ public class LeaderboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // static data in place for testing
-        ArrayList<LeaderboardActivity.LeaderboardItem> leaderboardItems = new ArrayList<>(Arrays.asList(
-                new LeaderboardItem(1, "user_123", 10, 1),
-                new LeaderboardItem(2, "user_456", 30, 3),
-                new LeaderboardItem(3, "user_789", 20, 2))
-        );
+        ArrayList<LeaderboardActivity.LeaderboardItem> leaderboardItems = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
         recyclerView = findViewById(R.id.leaderboardRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        leaderboardItems.sort((Comparator.comparing(LeaderboardItem::getUserReviewCount)).reversed());
+        mDatabase.child("users").get().addOnCompleteListener(t -> {
+            if (!t.isSuccessful()) {
+                Log.e("firebase", "Error getting data", t.getException());
+            } else {
 
-        recyclerViewAdapter = new LeaderboardRecyclerViewAdapter(LeaderboardActivity.this, leaderboardItems);
-        recyclerView.setAdapter(recyclerViewAdapter);
+                for (DataSnapshot dschild : t.getResult().getChildren()) {
+                    String username = String.valueOf(dschild.getKey());
+
+                    int reviewCount = Integer.parseInt(String.valueOf(dschild.child("reviewCount").getValue()));
+                    String badgeStatus = String.valueOf(dschild.child("badgeStatus").getValue());
+                    leaderboardItems.add(new LeaderboardItem(username, reviewCount, badgeStatus));
+                    Log.e("LEADERBOARDITEMS", String.valueOf(leaderboardItems));
+                }
+                Log.e("LEADERBOARDITEMSDONE", String.valueOf(leaderboardItems));
+
+
+                leaderboardItems.sort((Comparator.comparing(LeaderboardItem::getUserReviewCount)).reversed());
+
+                recyclerViewAdapter = new LeaderboardRecyclerViewAdapter(LeaderboardActivity.this, leaderboardItems);
+                recyclerView.setAdapter(recyclerViewAdapter);
+
+            }});
+
+
 
     }
 }
