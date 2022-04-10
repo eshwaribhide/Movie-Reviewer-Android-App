f595c06f502eae7910e261f379c1bdb1a1757ee3
Eshwari Bhide <ebhide99@gmail.com>
2022-04-10 00:17:34 -0400

    Add review functionality, storing review and movie association in db

--- app/src/main/java/edu/neu/madcourse/numad22sp_moviereviewer_teamdedj/MovieDetailsActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_moviereviewer_teamdedj/MovieDetailsActivity.java
@@ -1,19 +1,37 @@
 package edu.neu.madcourse.numad22sp_moviereviewer_teamdedj;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+// need username
+
 public class MovieDetailsActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    private String movieID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         getIncomingIntent();
     }
@@ -28,7 +46,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
             String movieReleaseDate = getIntent().getStringExtra("movie_release_date");
             String movieDescription = getIntent().getStringExtra("movie_description");
 
+            this.movieID = movieID;
             setMovieDetails(movieTitle, moviePoster, movieReleaseDate, movieDescription);
+
         }
     }
 
@@ -46,4 +66,40 @@ public class MovieDetailsActivity extends AppCompatActivity {
         TextView movieDescriptionView = findViewById(R.id.movie_details_description);
         movieDescriptionView.setText(movieDescription);
     }
+
+    public void reviewMovieBtnOnClick(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Write A Review");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText editReview = new EditText(this);
+        editReview.setHint("Write Review");
+        layout.addView(editReview);
+
+        editReview.setTextColor(Color.parseColor("#9C27B0"));
+
+        alertDialogBuilder.setView(layout);
+        alertDialogBuilder.setPositiveButton("OK", (dialog, whichButton) -> {
+            mDatabase.child("movies").child(movieID).get().addOnCompleteListener(t1 -> {
+                // The date is the review ID
+                String date = new Date().toString();
+                if (t1.getResult().getValue() == null) {
+                    mDatabase.child("movies").child(movieID).setValue(movieID);
+                }
+                mDatabase.child("movies").child(movieID).child("reviews").child(date).setValue(date);
+                mDatabase.child("reviews").child(date).setValue(editReview.getText().toString());
+
+            });
+        });
+
+
+        alertDialogBuilder.setNegativeButton("Cancel", (dialog, whichButton) -> {
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+}
 }
